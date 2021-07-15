fcfa691b2076255d0e428f801071ecad79d8c3b7
Marco <marcoparra311@gmail.com>
2021-07-15 15:25:22 -0400

    Commit to merge

--- src/modules/tasks.js
+++ src/modules/tasks.js
@@ -4,6 +4,21 @@ class Task {
     this.completed = completed;
     this.index = index;
   }
+
+  static displayTask = (tasks, list) => {
+    tasks.forEach((task) => {
+      const li = document.createElement('li');
+      li.setAttribute('draggable', 'true');
+      li.dataset.id = `${task.index}`;
+      li.classList.add('d-flex', 'align-items-center', 'justify-content-between', 'p-3', 'border-top', 'draggables');
+      li.innerHTML = `<div class="d-flex align-items-center">
+                        <input type="checkbox" class="me-2 check">
+                        <span class="border-0" contenteditable="true"> ${task.description} </span>
+                      </div> 
+                      <i class="fas fa-ellipsis-v"></i>`;
+      list.appendChild(li);
+    });
+  };
 }
 
 export { Task };
